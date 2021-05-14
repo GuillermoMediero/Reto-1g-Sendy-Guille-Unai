9a0bcb24a4ccb72e3c5cebf4ec1f7f17631b6007
unai <unai.diez@ikasle.egibide.org>
2021-05-14 09:26:58 +0200

    javo

--- Projecto_unai/src/Modelo_BBDD/TUsuario.java
+++ Projecto_unai/src/Modelo_BBDD/TUsuario.java
@@ -5,14 +5,41 @@
  */
 package Modelo_BBDD;
 
+import Modelo_UML.Administrador;
+import Modelo_UML.Usuario;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author ketro
  */
 public class TUsuario {
-
-    public static boolean Conectarusu(String correo, String clave) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+private static PreparedStatement sentenciaPre;
+    private static String plantilla;
+    private static ResultSet resultado;
+    private static Usuario a;
     
+    public static Boolean Conectarusu(String correo,String contrasena) throws SQLException {
+         
+        BaseDatos.conectar();
+            
+        plantilla = "select * from usuario where CORREO = ? AND CONTRASENA= ?";
+        sentenciaPre =BaseDatos.getCon().prepareStatement(plantilla);
+        sentenciaPre.setString(1,correo);
+        sentenciaPre.setString(2,contrasena);
+        resultado = sentenciaPre.executeQuery();
+        if (resultado.next())
+        {      
+            BaseDatos.desconectar();
+            return true;
+        }
+         else
+         {      
+            BaseDatos.desconectar(); 
+             return false;   
+         }
+        
+    }
 }
