e79f751b66e28bac5ebcd822bc25ef8f3a93af46
unai <unai.diez@ikasle.egibide.org>
2021-05-18 18:25:23 +0200

    sql 2.2

--- SQL/paquete_F2.sql
+++ SQL/paquete_F2.sql
@@ -117,33 +117,37 @@ v_bucle number(2):=1;
 
 
 
- create or replace  PROCEDURE  crearcalendario AS        
+create or replace  PROCEDURE  crearcalendario AS        
     BEGIN
         DECLARE
              CURSOR C
              IS 
-             SELECT e1.ID_EQUIPO AS"eq_local",e2.ID_EQUIPO AS"eq_visitante" FROM equipo e1, equipo e2 WHERE e1.ID_EQUIPO <> e2.ID_EQUIPO;
-             CURSOR D IS
-             SELECT * FROM JORNADA ;
+             SELECT e1.ID_EQUIPO AS eq_local, e2.ID_EQUIPO AS eq_visitante
+             FROM equipo e1, equipo e2 
+             WHERE e1.ID_EQUIPO <> e2.ID_EQUIPO;
+             v_cursor C%ROWTYPE;
+             
+             CURSOR D 
+             IS
+             SELECT NUM_JORNADA 
+             FROM JORNADA ;
              v_jornada D%ROWTYPE;
-            v_cursor C%ROWTYPE;
-
+             v_partido partido.ID_PARTIDO%TYPE;
         v_cant number;
         BEGIN
-        SELECT count(*)into v_cant FROM jornada;
-        if (v_cant="0")then
-        execute crearjornadas();
         FOR v_jornada IN D
            LOOP
              FOR v_cursor IN C
            LOOP
-           select * from partidos WHERE NUM_JORNADA=v_jornada.NUM_JORNADA AND 
-           ID_EQUIPOL=v_cursor.eq_local or ID_EQUIPOL=v_cursor.eq_visitante OR
-           ID_EQUIPOV=v_cursor.eq_local or ID_EQUIPOV=v_cursor.eq_visitante;
-           if no_data_found then
+           select ID_PARTIDO into v_partido
+           from partido p, jornada j 
+           WHERE p.NUM_JORNADA=v_jornada.NUM_JORNADA 
+           AND ID_EQUIPOL  in (v_cursor.eq_local , v_cursor.eq_visitante)
+           OR ID_EQUIPOV  in (v_cursor.eq_local , v_cursor.eq_visitante);
+           IF SQL%NOTFOUND then
            INSERT INTO PARTIDO(HORA, RESULTADOL,resultadov,num_jornada,id_equipol,id_equipov) VALUES('12/06/21 18:50:00',null,null,v_jornada.NUM_JORNADA,v_cursor.eq_local,v_cursor.eq_visitante);
-           end if;
+           end IF;
            END LOOP;
           END LOOP; 
-        END;
+       END;
      END;
